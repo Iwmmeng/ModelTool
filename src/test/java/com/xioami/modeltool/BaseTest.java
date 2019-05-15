0364f23c7b1c279181b36e42ef5a2dd99a2a83d3
Iwmmeng <834638005@qq.com>
2019-05-15 14:06:39 +0800

    first commit

---
+++ src/test/java/com/xioami/modeltool/BaseTest.java
@@ -0,0 +1,14 @@
+package com.xioami.modeltool;
+
+public class BaseTest {
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/xioami/modeltool/util.java
@@ -0,0 +1,62 @@
+package com.xioami.modeltool;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class util {
+    JSONObject response;
+/**
+ *{"deviceType号": [{"propertyType号1":
+                                    [{"format": "XXX"}, {"unit": "XXX"}, {"valueRange": [0, 100, 1]}]}]}
+
+ *{"deviceType号": [{"propertyType号1":
+ [{"format": "XXX"}, {"unit": "XXX"}, {"valueRange": [0, 100, 1]}]}]}
+ *
+ *
+ * {"deviceType号": [{"propertyType号1":
+ ["format": "XXX", "unit": "XXX", "valueRange": [0, 100, 1]]}]}
+ * */
+    public void getNotifyProperty() throws JSONException {
+
+        JSONObject notifyJsonObject = new JSONObject();
+        JSONObject propertyJsonObject = new JSONObject();
+        JSONObject jsonObject2 = new JSONObject();
+        JSONArray notifyJsonArray = new JSONArray();
+        JSONArray propertyJsonArray = new JSONArray();
+        jsonObject.put("",jsonObject2);
+
+
+
+        if(response.getBoolean("services")){
+            JSONArray servicesArray = new JSONArray(response.getString("services"));
+            for(int i =0;i<servicesArray.length();i++){
+                if(servicesArray.getJSONObject(i).getBoolean("properties")){
+                    JSONArray propertiesArray = new JSONArray(servicesArray.getJSONObject(i));
+                    for(int j=0;j<propertiesArray.length();j++){
+                        if(propertiesArray.getJSONObject(j).getBoolean("access")){
+                            JSONArray accessArray = propertiesArray.getJSONObject(i).getJSONArray("access");
+                            for(int k=0;k<accessArray.length();k++){
+                                if(accessArray.getString(i).equals("notify")){
+
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+
+
+
+        }
+    }
+
+//    if()
+//    for(){
+//
+//    }
+
+}
